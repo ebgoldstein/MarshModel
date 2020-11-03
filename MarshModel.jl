61a3ffa4c1ceb038fa5b4b21e71b1b7a753597e5
Evan B. Goldstein <ebgoldstein@gmail.com>
2020-11-02 19:24:54 -0500

    params

--- MarshModel.jl
+++ MarshModel.jl
@@ -9,31 +9,41 @@ function NonAMarsh(dM,M,p,t)
     #intrinsic growth rate
     SL,r = p
 
-    #set the params (Alizada et al 2016)
-    #Plant params, in g/cm^2 (max biomass of 0.1088 g/cm2)
-    a = 0.1000
-    b = -0.3718
-    c = 0.1021
+    #set the params (from Morris et al 2002)
+    #max biomass is ~1800 g/m^2
+    #Plant params, in g/m^2
+    a = 15500
+    b = -18550
+    c = -1364
 
-    #dep params (in cm/yr)
+    TooShallow = .10 
+    TooDeep = .73
+
+    #dep params (in m/yr)
     q = 0.0018
-    k = 2.5*(10^-5)
+    k = 1.5*(10^-5)
 
-    #model EQNS
+    #carrying capacity
+    κ = (a*M[2]) + (b*M[2]*M[2]) + (c)
+
     #plant biomass EQN
-    dM[1] = r*M[1]*(1.0 - (M[1]/((a*M[2]) + (b*M[2]*M[2]) + (c))))
+    dM[1] = r*M[1]*(1.0 - (M[1]/κ))
     #marsh platform depth EQN w/ SLR
     dM[2] = SL - (q+(k*M[1]))*M[2]
 end
 
-M0 = [0.01, 0.3]
-p = (0.001,0.01)
-tspan = (0.0,10000.0)
+#parameters (SLR, r)
+p = (0.01,0.1)
+#ICs (biomass, depth)
+M0 = [100, .1]
+
+#time span
+tspan = (0.0,1000.0)
 prob = ODEProblem(NonAMarsh,M0,tspan,p)
 sol = solve(prob)
 
-p1 = plot(sol,vars=(0,1), label = "Biomass", xlabel = "Time (yr)", ylabel = "Biomass (g/cm^2)",lw = 3, legend = true)
-p2 = plot(sol,vars=(0,2), label = "Depth", xlabel = "Time (yr)", ylabel = "Depth (cm)", lw = 3, legend = true)
-p3 = plot(sol,vars=(1,2), title= "phase space", xlabel = "Biomass (g/cm^2)", ylabel = "Depth (cm)", legend = false, lw = 3)
+p1 = plot(sol,vars=(0,1), label = "Biomass", xlabel = "Time (yr)", ylabel = "Biomass (g/m^2)",lw = 3, legend = true)
+p2 = plot(sol,vars=(0,2), label = "Depth", xlabel = "Time (yr)", ylabel = "Depth (m)", lw = 3, legend = true)
+p3 = plot(sol,vars=(1,2), title= "phase space", xlabel = "Biomass (g/m^2)", ylabel = "Depth (m)", legend = false, lw = 3)
 
 plot(p1, p2, p3, layout = (3, 1))
