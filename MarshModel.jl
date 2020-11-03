bb60fa9dc0351a561772efc22f756dae74e8ecb1
Evan B. Goldstein <ebgoldstein@gmail.com>
2020-11-03 14:16:48 -0500

    new block/trap ( #1 )

--- MarshModel.jl
+++ MarshModel.jl
@@ -3,7 +3,6 @@ using DifferentialEquations
 using Plots
 
 function NonAMarsh(dM,M,p,t)
-
     #params
     #intrinsic growth rate
     SL,r = p
@@ -22,20 +21,34 @@ function NonAMarsh(dM,M,p,t)
     #carrying capacity based on Morris et al 2002
     κ = (a*M[2]) + (b*M[2]*M[2]) + (c)
 
-    #time varying biomass to use for depth eqn
-    Bt = abs(sin(π*t))* M[1]
+    #BIOMASS EQN
+    #time varying biomass to use for depth eqn 
+    #biomass goes to 0
+    #Bt = abs(sin(π*t))* M[1]
+    #biomass goes varies between 1 and 0.5
+    Bt = M[1] * ((0.25)*sin(2*π*t)+0.75)
+    #biomass is constant
+    #Bt = M[1]
+
+    #trapping EQN
+    # at 0 biomass, 0 efficiency
+    # at 4000 biomass, 0 efficiency
+    # at 2000 biomass, 100% efficiency
+    TE = (M[1]/1000) - ((M[1]*M[1])/4000000)
+    #no blocking trapping term
+    #TE = 1
 
     #plant biomass EQN
     dM[1] = r*M[1]*(1.0 - (M[1]/κ))
 
     #marsh platform depth EQN w/ SLR
-    dM[2] = SL - (q+(k*Bt))*M[2]
+    dM[2] = SL - (q+(TE*k*Bt))*M[2]
 
 end
 
 #parameters (SLR, r)
 #SLR is Charleston,SC is 3.32 mm/yr or 0.0032
-p = (0.006, 0.1)
+p = (0.01, 0.1)
 #ICs (biomass, depth)
 M0 = [100, .2]
 
